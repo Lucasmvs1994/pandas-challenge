8a2ccb047fc6adfae3f8038eb0d28c492dc1b982
Lucas Vianna <Lucas.Vianna@fleetcor.com>
2021-10-06 15:04:56 -0400

    COMMIT

--- HeroesOfPymoli/Analysis/analysis.py
+++ HeroesOfPymoli/Analysis/analysis.py
@@ -3,4 +3,4 @@ filename = 'analysis.txt'
 with open(filename, 'w') as file_object:
     file_object.write("1. The top 3 most profitable items (by purchase value) are Final Critic, Oathbreaker, and Nirvana.\n\n")
     file_object.write("2. Male players tend to buy and spend more on games. \n\n")
-    file_object.write("3. Players between ages  20-24 years tend to spend money on games.\n\n")
+    file_object.write("3. Players between ages  20-24 years tend to spend more money on games.\n\n")
